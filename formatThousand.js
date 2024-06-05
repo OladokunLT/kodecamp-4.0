20c1cab8dc5867bd517046527b30fef23ef68930
oladokunlt <lukmanoladokun86@gmail.com>
2024-06-05 14:15:33 +0100

    Finished fomatThousand function

--- formatThousand.js
+++ formatThousand.js
@@ -1,4 +1,8 @@
 function formatThousand(num) {
-  return num;
+  formatedNum = num.toLocaleString();
+  return formatedNum;
 }
-console.log(formatThousand(1000000));
+formatThousand(1000000);
+console.log(formatThousand(10000000));
+console.log(formatThousand(1234503.05));
+console.log(formatThousand(12345.05));
